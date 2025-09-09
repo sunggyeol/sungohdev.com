60e15390e64fd168e4a175f73d149db0095ba17c
Sung Oh <sunggyeol@vt.edu>
2025-09-09 16:39:35 -0400

    fix: mobile scrolling issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
             analyticsConfig={siteMetadata.analytics as AnalyticsConfig}
           />
           <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
+            <div className="flex min-h-screen flex-col justify-between font-sans">
               <SearchProvider
                 searchConfig={siteMetadata.search as SearchConfig}
               >
